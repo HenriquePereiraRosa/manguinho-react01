443c2e5eee4bf1866c27ef15eb3a7b79a4938a20
Henrique P. Rosa <henrique.prosa@hotmail.com>
2022-04-17 22:27:56 +0100

    refac: create makeSut factory for RemoteAuth :tada:

--- src/data/feature/auth/remote-auth.spec.ts
+++ src/data/feature/auth/remote-auth.spec.ts
@@ -1,12 +1,24 @@
 import { PostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuth } from './remote-auth.'
 
+type SutTypes = {
+  sut: RemoteAuth,
+  postClientSpy: PostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const postClientSpy = new PostClientSpy()
+  const sut = new RemoteAuth(url, postClientSpy)
+  return {
+    sut,
+    postClientSpy
+  }
+}
 
 describe('RemoteAuth', () => {
   it('Should call PostClient with correct URL', async () => {
     const url = 'any_url'
-    const postClientSpy = new PostClientSpy()
-    const sut = new RemoteAuth(url, postClientSpy)
+    const { sut, postClientSpy } = makeSut(url)
     await sut.auth()
     expect(postClientSpy.url).toBe(url)
   })
