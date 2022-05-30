de06431f615e03c61d13df94a14913096e98161e
Henrique P. Rosa <henrique.prosa@hotmail.com>
2022-05-31 00:02:27 +0100

    feat: ensure AxiosHttpClient calls axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,16 @@
+import faker from "@faker-js/faker"
+import axios from "axios"
+import { AxiosHttpClient } from "./axios-http-client"
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('Axios Http Client', () => {
+
+  it('Should call Axios with correct Url', async () => {
+    const url = faker.internet.url()
+    const sut = new AxiosHttpClient()
+    await sut.post({ url })
+    expect(mockedAxios).toHaveBeenCalledWith(url)
+  })
+})
---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,8 @@
+import { PostClientParams } from "@/data/protocols/http";
+import axios from "axios";
+
+export class AxiosHttpClient {
+  async post(params: PostClientParams<any>): Promise<void> {
+    await axios(params.url)
+  }
+}
