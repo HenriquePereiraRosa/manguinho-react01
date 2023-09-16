8f3bce776a83e2373d2cbddd183e31875f021b1c
henrique <henrique.prosa@hotmail.com>
2023-09-16 19:46:04 +0100

    feat.0030: moved factories to their own files

---
+++ src/main/factories/http/api-url-factory.ts
@@ -0,0 +1,4 @@
+
+export const makeApiUrl = (): string => {
+  return 'http://localhost:3001/api/login'
+}
---
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,9 @@
+import { RemoteAuth } from '@/data/feature/auth/remote-auth'
+import { makeAxiosHttpClient } from '../../http/axios-http-client-factory'
+import { type IAuthentication } from '@/domain/feature/auth'
+import { makeApiUrl } from '../../http/api-url-factory'
+
+export const makeRemoteAuth = (): IAuthentication => {
+  const apiUrl = makeApiUrl()
+  return new RemoteAuth(apiUrl, makeAxiosHttpClient())
+}
